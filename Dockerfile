e1852d8d09f04ffd36a1c5875172b8e93845c43b
MxLourier <maya.v.lourie@gmail.com>
2026-02-13 12:46:21 +0200

    Inintial code

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM ghcr.io/wisetrout/playwright-ui-tests-menu:latest
+WORKDIR /app
+EXPOSE 3000 9323
---
+++ tests/dummy.spec.js
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+import { test } from '../ui-lib/fixtures.js';
+import createDescribe from '../ui-lib/describe.js';
+import { readFileSync } from 'fs';
+import { parse } from 'csv-parse/sync';
+
+const describe = createDescribe("dummy.spec.js");
+parse({  })
+
+describe('Dummy category', () => {
+        test('Page title', async ({ page }) => {
+            await page.goto('/');
+            await expect(page).toHaveTitle('What is this site? | Wise Trout');
+        })
+});
+
+describe('Using CSV module', () => {
+    const csv = readFileSync('metadata/suites/dummy-suite/data.csv');
+    const records = parse(csv, {
+        columns: true,
+        skip_empty_lines: true
+    })
+    records.forEach(record => {
+        test(record.label, async ({page}) => {
+            await page.goto(record.URL);
+            await expect(page).toHaveTitle(record.title  + ' | Wise Trout');
+        })
+    })
+});
